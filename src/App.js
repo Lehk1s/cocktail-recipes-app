e3d5caf4ae4cd472dade035a967d7933e3880935
Lehk1s <122699536+Lehk1s@users.noreply.github.com>
2023-10-15 00:06:26 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const API_URL = 'https://www.thecocktaildb.com/api/json/v1/1';
+const RANDOM_API_ENDPOINT = '/random.php';
+const SEARCH_API_ENDPOINT = '/search.php?s=';
+
 function App() {
+  const [cocktail, setCocktail] = useState(null);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const fetchRandomCocktail = async () => {
+    try {
+      const response = await fetch(API_URL + RANDOM_API_ENDPOINT);
+      const data = await response.json();
+      setCocktail(data.drinks[0]);
+    } catch (error) {
+      console.error('Error fetching random cocktail:', error);
+    }
+  };
+
+  const searchCocktail = async () => {
+    try {
+      const response = await fetch(API_URL + SEARCH_API_ENDPOINT + searchTerm);
+      const data = await response.json();
+      setSearchResults(data.drinks || []);
+    } catch (error) {
+      console.error('Error searching cocktail:', error);
+    }
+  };
+  
+  const handleCocktailClick = (selectedCocktail) => {
+    setCocktail(selectedCocktail);
+    setSearchResults([]);
+  };
+  
+
+  useEffect(() => {
+    fetchRandomCocktail();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Cocktail Recipes</h1>
+      <div>
+        <button onClick={fetchRandomCocktail}>Get Random Cocktail</button>
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Search Cocktail"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button onClick={searchCocktail}>Search</button>
+      </div>
+      {cocktail && (
+        <div className="cocktail-details">
+          <h2>{cocktail.strDrink}</h2>
+          <img src={cocktail.strDrinkThumb} alt={cocktail.strDrink} />
+          <p>{cocktail.strInstructions}</p>
+        </div>
+      )}
+{searchResults.length > 0 && (
+  <div className="search-results">
+    <h2>Search Results</h2>
+    <ul>
+      {searchResults.map((result) => (
+        <li key={result.idDrink} onClick={() => handleCocktailClick(result)}>
+          {result.strDrink}
+        </li>
+      ))}
+    </ul>
+  </div>
+)}
     </div>
   );
 }
